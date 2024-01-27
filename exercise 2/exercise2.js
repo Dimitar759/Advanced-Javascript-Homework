93f112f61cdbe3ad0b5cbc41a5edf1e8790ee9a3
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2024-01-27 22:44:28 +0100

    Exercise 1 finished

---
+++ exercise 2/exercise2.js
@@ -0,0 +1,23 @@
+fetch("https://dummyjson.com/products")
+.then(function (response) {
+  return response.json();
+})
+.then(function (response) {
+  console.log(laptopsDescendingByPrice(response.products))
+  console.log(firstGroceryItem(response.products))
+})
+.catch(function (response) {
+  console.log("failed request " + response.status);
+});
+
+const laptopsDescendingByPrice = response => {
+  let laptops = response.filter(gadget => gadget.category === "laptops");
+  laptops.sort((a, b) => b.price - a.price);
+
+  return laptops;
+}
+
+const firstGroceryItem = response => {
+   let firstItemIndex = response.findIndex(product => product.category === "groceries");
+  return firstItemIndex;
+}
